80ba908215aa614910b40a22b8dcecef4daccdef
aaronlyy <aaronlevican@gmail.com>
2022-01-06 15:34:12 +0100

    added directory support, added exit codes, added output after run, added single switch

--- check_vhdx_size.ps1
+++ check_vhdx_size.ps1
@@ -2,15 +2,10 @@
 # can be used with single file or complete directory
 
 Param(
-    [Parameter(HelpMessage="Absolute path to a single vhdx disk")]
-    [Alias("f")]
+    [Parameter(Mandatory, HelpMessage="Path to a directory of multiple vhdx disks or a absolute path to a single disk")]
+    [Alias("p")]
     [String]
-    $File = $null,
-
-    [Parameter(HelpMessage="Path to a directory of multiple vhdx disks")]
-    [Alias("d")]
-    [String]
-    $Directory = $null,
+    $Path = $null,
 
     [Parameter(Mandatory, HelpMessage="Max disk size in Gb")]
     [Alias("m")]
@@ -20,7 +15,12 @@ Param(
     [Parameter(HelpMessage="Warning space limit as % (0.5, 0.95, etc.)")]
     [Alias("w")]
     [Float]
-    $Warning = 0.95
+    $Warning = 0.95,
+
+    [Parameter(HelpMessage=("Set to true if given path is a single file"))]
+    [Alias("s")]
+    [Switch]
+    $Single
 )
 
 function Get-Size {
@@ -46,46 +46,97 @@ function Get-Size {
     }
 }
 
-$results = @{}
-$code = 0
-$warningGb = $Max * $Warning
+$resOk = @{}
+$resWarning = @{}
+$resCritical = @{}
 
-if ($File -Ne $null -And $File -Ne ""){
-    # passed parameter is a single file
-    # get file size in gb
-    $sizeGb = Get-Size $File
+$warningGb = $Max * $Warning # calculate the warning gb
+
+if ($Single.isPresent){
+    # single switch is set, get only one file
+    $sizeGb = Get-Size $Path
+    $sizeGbRound = [math]::Round($sizeGb,2)
     # check if size -Ne $null to validate success
     if ($sizeGb -Ne $null){
         # check size against limit
         if ($sizeGb -Ge $Max){
             # size is -Ge to Disk, add path and critical to table
-            $results[$File] = "100% usage! ($sizeGb/$max)"
+            $resCritical[$Path] = $sizeGbRound
         }
         elseif ($sizeGb -Ge $warningGb){
             # limit is greater or equal to limitGb, add path and warning
-            $results[$File] = "$Warning usage exceeded! ($sizeGb/$max)"
+            $resWarning[$Path] = $sizeGbRound
         }
         else {
             # everytings fine, add path and ok to table
-            $results[$File] = "Ok ($sizeGb/$max)"
+            $resOk[$Path] = $sizeGbRound
         }
     }
     else {
-        $code = -1
-        $results[$File] = "Error getting file"
+        $resWarning[$Path] = "Error getting file size"
     }
 }
-elseif ($Directory -Ne $null -And $Directory -Ne ""){
-    # passed parameter is directory
-    # loop through directory
-    continue
-}
 else {
-    Write-Host "Missing Parameter file or directory path!"
-    Write-Host "Usage: .\check_vhdx -File f.vhdx -Disk 20 -Warning 0.95"
-    $code = -1
+    # passed path is a directory
+    $files = Get-ChildItem $Path -Filter "*.vhdx" | ForEach { $_.FullName }
+    foreach ($f in $files) {
+        $sizeGb = Get-Size -f $f
+        $sizeGbRound = [math]::Round($sizeGb,2)
+        if ($sizeGb -Ne $null){
+        # check size against limit
+            if ($sizeGb -Ge $Max){
+                # size is -Ge to Disk, add path and critical to table
+                $resCritical[$f] = $sizeGbRound
+            }
+            elseif ($sizeGb -Ge $warningGb){
+                # limit is greater or equal to limitGb, add path and warning
+                $resWarning[$f] = $sizeGbRound
+            }
+            else {
+                # everytings fine, add path and ok to table
+                $resOk[$f] = $sizeGbRound
+            }
+        }
+        else {
+            $resWarning[$f] = "Error getting file"
+        }
+    }
 }
 
-$str = $results | Out-String
-Write-Host $str -ForegroundColor Green
-return $code
+if ($resWarning.Count -Gt 0 -And $resCritical.Count -Gt 0) {
+    $code = 2
+    foreach ($f in $resWarning.getEnumerator()){
+        $name = $f.Name
+        $value = $f.Value 
+        Write-Host "Warning: $name - $value/$Max (Gb)"
+    }
+
+    foreach ($f in $resCritical.getEnumerator()){
+        $name = $f.Name
+        $value = $f.Value
+        Write-Host "Critical: $name - $value/$Max (Gb)"
+    }
+
+}
+elseif ($resWarning.Count -Gt 0){
+    $code = 1
+    foreach ($f in $resWarning.getEnumerator()){
+        $name = $f.Name
+        $value = $f.Value
+        Write-Host "Warning: $name - $value/$Max (Gb)"
+    }
+}
+elseif ($resCritical.Count -Gt 0){
+    $code = 2
+    foreach ($f in $resCritical.getEnumerator()){
+        $name = $f.Name
+        $value = $f.Value
+        Write-Host "Critical: $name - $value/$Max (Gb)"
+    }
+}
+else {
+    $code = 0
+    Write-Host "Ok"
+}
+
+exit($code)
